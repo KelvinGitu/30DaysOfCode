74aa859bf7c62e1e50211f01b018b08ad9365f54
Kelvin Gitu <52266046+KelvinGitu@users.noreply.github.com>
2020-05-01 21:52:48 +0300

    Add files via upload

---
+++ average.py
@@ -0,0 +1,17 @@
+def average():
+    
+    numbers = str(input("Enter a string of numbers: "))
+    numbers = numbers.split()
+
+    numbers2 = []
+    total = 0
+    
+    for number in numbers:
+        number = int(number)
+        total += number
+        numbers2.append(number)
+
+    avg = total // len(numbers2)
+    print(avg)
+
+average()
