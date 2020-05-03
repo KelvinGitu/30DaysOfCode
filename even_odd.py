f47ad553eb2efbb5de038a627114f5c17b9cf849
KelvinGitu <gitukelvin01@gmail.com>
2020-05-03 23:48:37 +0300

    Day 3

--- even_odd.py
+++ even_odd.py
@@ -1,8 +1,8 @@
 def even_odd(list_of_num):
-    
+
     even_num = list(filter(lambda number: number % 2 == 0, numbers))
     print("Number of even numbers: ", len(even_num))
-
+    
     odd_num = list(filter(lambda number: number % 2 != 0, numbers))
     print("Number of odd numbers: ", len(odd_num))
 
