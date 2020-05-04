ba8caa62d4105fd26e7cfadf25ed8529ec712a92
KelvinGitu <gitukelvin01@gmail.com>
2020-05-04 19:50:52 +0300

    Day 4

---
+++ bits_of_gray.py
@@ -0,0 +1,8 @@
+def bits_of_gray():
+    num_of_bits = int(input("Enter the number of bits: "))
+    print(num_of_bits)
+    
+
+
+
+bits_of_gray()
---
+++ lower_upper_case.py
@@ -0,0 +1,19 @@
+def string_test(string_sentence):
+    upper_case = 0
+    lower_case = 0
+    for letter in sentence:
+        # define punctuation
+        punctuations = """ !()-[]{ };:'"\,<>./?@#$%^&*_~ """
+        # remove punctuation from the string
+        no_punct = ""
+        if letter not in punctuations:
+            if letter == letter.upper() and letter != '':
+                upper_case += 1
+            elif letter == letter.lower() and letter != '':
+                lower_case += 1
+
+    print("Number of uppercase letters is: ", upper_case)
+    print("Number of lowercase letters is: ", lower_case)
+            
+sentence = "My name is Bruce BANNER and I'm an Avenger."
+string_test(sentence)
