144589e50ecc83bf2f9c6fec26a46fe6e3ca862b
KelvinGitu <gitukelvin01@gmail.com>
2020-05-06 22:10:58 +0300

    Day 6

---
+++ prime_sum.py
@@ -0,0 +1,17 @@
+#the program returns asum of prime numbers between 1 and an integer taken as the input
+
+def primeSum(num):
+
+    total = 0
+    for number in range(1, integer_num + 1):
+        if 1 < number < 1000000:
+            for n in range(2, number):
+                if number % n == 0:
+                    break
+            else:
+                total += number
+
+    print(total)
+
+integer_num = int(input("Integer number: "))
+primeSum(integer_num)
