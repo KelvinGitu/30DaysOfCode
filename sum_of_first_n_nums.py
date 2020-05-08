698cc4eab79dd0a9cc1ad1de627751992b1c44ec
KelvinGitu <gitukelvin01@gmail.com>
2020-05-08 20:29:47 +0300

    Day 8

---
+++ sum_of_first_n_nums.py
@@ -0,0 +1,8 @@
+def fastSum(number):
+    """ the function takes a parameter, n and returns 
+    the sum of the first n numbers """
+    
+    return int(num * (num + 1) / 2)
+
+num = int(input("Enter number: "))
+fastSum(num)
